7099459a18a480d0f16b2d560dcc86d49af9fc87
赵坤 <zk9474@hotmail.com>
2020-04-06 16:18:34 +0800

    update vue.config.js

--- src/vue/vue.config.js
+++ src/vue/vue.config.js
@@ -21,7 +21,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/abp-vue/'
+    : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
