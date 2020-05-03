1bee5ea8334c0af06e3ef63e5219001c5b939dec
赵坤 <zk9474@hotmail.com>
2020-05-03 11:37:15 +0800

    update

--- src/vue/build/build.sh
+++ src/vue/build/build.sh
@@ -3,16 +3,16 @@
 cd /abpvue/vue/
 ls -l
 
-docker build -t abpvue_frontend:v$BUILD_NUMBER .
-docker tag abpvue_frontend:v$BUILD_NUMBER abpvue_frontend:latest
+docker build -t abpvue_frontend:v$1 .
+docker tag abpvue_frontend:v$1 abpvue_frontend:latest
 
 # 停止
-if ["$(docker ps -q -f name=abpvue_frontend)"]; then
+if [ "$(docker ps -q -f name=abpvue_frontend)" ]; then
     docker stop abpvue_frontend
 fi
 
 # 删除
-if ["$(docker ps -qa -f name=abpvue_frontend)"]; then
+if [ "$(docker ps -qa -f name=abpvue_frontend)" ]; then
     docker rm abpvue_frontend
 fi
 
